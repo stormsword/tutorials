bdcaf7e26f1016a7517c5c1144a57fa6b61ef0b0
bdickason <dickason@gmail.com>
2014-08-22 23:21:03 -0400

    Enemies can wander around randomly

--- command-stack/Assets/scripts/characters/enemies/EnemyScript.cs
+++ command-stack/Assets/scripts/characters/enemies/EnemyScript.cs
@@ -6,12 +6,11 @@ public class EnemyScript : MonoBehaviour {
 	// Player - Search for and store the player so we can chase it
 	private GameObject player;
 
-	// Chase Command Script - Charge at an enemy
-	ChaseCommandScript command;
+	// Wander Command Script - Wander around aimlessly
+	WanderCommandScript command;
 	
 	void Start() {
-		Debug.Log (this.gameObject.name);
-		command = new ChaseCommandScript(this.gameObject);
+		command = new WanderCommandScript(this.gameObject);
 		player = GameObject.FindGameObjectWithTag("Player");	// Make sure your player has the 'Player' tag!
 		command.target = player;
 	
---
+++ command-stack/Assets/scripts/characters/enemies/WanderCommandScript.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* WanderCommand - Wanders about aimlessly */
+public class WanderCommandScript : CommandScript {
+	
+	private MoveScript moveScript;
+
+	private Vector2 destination;
+	
+	/* WanderCommandScript - Constructur called when Command is first created 
+	 _character - the Character that the Command Stack is attached to */
+	public WanderCommandScript(GameObject _character) : base(_character) {
+		moveScript = character.GetComponent<MoveScript>();
+	}
+	
+	/* Execute is usually called once per frame */
+	public override void Execute () {
+		destination = GetRandomDestination();
+		moveScript.Move(destination);
+	}
+	
+	private Vector2 GetRandomDestination() {
+		return(Random.insideUnitCircle*5);	// Only wander within a small circular radius
+	}	
+}
